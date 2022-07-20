3bfb4fde15319276ab414275263ddc75903cba95
Marina Zelenevskaya <mzelianeuskaya@yandex.ru>
2022-07-20 21:22:01 +0300

    feat: add solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let items = bracketsConfig.map( item => item.join(''));
+
+  function rebase(str) {
+      for (let i =0; i < items.length; i++) {
+          if (str.includes(items[i])) {
+              str = str.replace(items[i], '');
+              return rebase(str);
+          } else if (!str.includes(items[i]) && str.length == 0) {
+              return true;
+          } else if (!str.includes(items[i]) && str.length < 0) {
+              return false;
+          }
+      }
+      return false;
+  }
+  return rebase(str);
 }
